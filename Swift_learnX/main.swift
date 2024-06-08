4d79b5e6682c921d5dc72e7b3f8ff7e467394eb2
chahalansh <anshchahal6543@gmail.com>
2024-06-08 23:55:07 +0530

    Datatypes also added

--- Swift_learnX/main.swift
+++ Swift_learnX/main.swift
@@ -12,18 +12,41 @@ print("Hello, World")
 //VARIABLES AND DATA TYPES:
 
 // There are basically 2 ways to declare values in swift:
-//    *First is using 'var' keywords representing variables:
+//    ~First is using 'var' keywords representing variables:
 var a = 12
 print(a)
 
 a = 17
 print (a)
 // #IMPORTANT# Always use wide space before and after the equal to (=) sign while declaring in Swift, else it will throw an error.
-//    *Second is using 'let' keyword representing a constant.
-let b = 15
-print(b)
+//    ~Second is using 'let' keyword representing a constant.
+let h = 15
+print("the value of h is:", h)
 
 // To declare a data type to a variable we use:
+var x: Int = 20
 
+// keywork: Datatype = value
+
+print(x)
+
+var s: String = "Ansh"
+
+var c: Character = "A"
+
+var i: Int = 09
+
+var d: Double = 9.09090909090909
+
+var f: Float = 9.090909
+
+var b: Bool = true
+
+print(s)
+print(c)
+print(i)
+print(d)
+print(f)
+print(b)
 
 
