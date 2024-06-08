e0c4cf4aecab3ac661e053b8c87b57f0060667c8
chahalansh <anshchahal6543@gmail.com>
2024-06-08 23:43:49 +0530

    Variables

--- Swift_learnX/main.swift
+++ Swift_learnX/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Hello, World")
+
+//VARIABLES AND DATA TYPES:
+
+// There are basically 2 ways to declare values in swift:
+//    *First is using 'var' keywords representing variables:
+var a = 12
+print(a)
+
+a = 17
+print (a)
+// #IMPORTANT# Always use wide space before and after the equal to (=) sign while declaring in Swift, else it will throw an error.
+//    *Second is using 'let' keyword representing a constant.
+let b = 15
+print(b)
+
+// To declare a data type to a variable we use:
+
+
 
